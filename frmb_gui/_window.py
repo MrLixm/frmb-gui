dbae1badea02c6a78b814f0bd8b33182dbba81f6
Liam Collod <lcollod@gmail.com>
2024-02-11 16:57:42 +0100

    refacto(window): add intermediate widgets for more control in stylesheet

--- frmb_gui/_window.py
+++ frmb_gui/_window.py
@@ -39,16 +39,38 @@ class FrmbHierarchyBrowserDock(QtWidgets.QDockWidget):
         self.setFeatures(self.DockWidgetFeature.NoDockWidgetFeatures)
 
 
+# we split an inner main window so the menu bar is not affected by the content margins
+class InnerMainWindow(QtWidgets.QMainWindow):
+    def __init__(self, parent: QtWidgets.QWidget = None):
+        super().__init__(parent)
+        self.control_bar_dock = FrmbControlBarDock()
+        self.hierarchy_dock = FrmbHierarchyBrowserDock()
+        self.setCentralWidget(self.hierarchy_dock)
+        self.addDockWidget(
+            QtCore.Qt.DockWidgetArea.TopDockWidgetArea, self.control_bar_dock
+        )
+
+
+# add an intermediate QFrame, so we can add margins via stylesheet
+class InnerCentralWidget(QtWidgets.QFrame):
+    def __init__(self, parent: QtWidgets.QWidget = None):
+        super().__init__(parent)
+        # 1. create
+        self.layout_main = QtWidgets.QVBoxLayout()
+        self.widget = InnerMainWindow()
+        # 2. build layout
+        self.setLayout(self.layout_main)
+        self.layout_main.addWidget(self.widget)
+        # 3. modify
+        self.layout_main.setContentsMargins(0, 0, 0, 0)
+
+
 class FrmbMainWindow(QtWidgets.QMainWindow):
     def __init__(self, parent: QtWidgets.QWidget = None):
         super().__init__(parent)
 
         self.menubar = MainMenuBar(self)
-        self.control_bar_dock = FrmbControlBarDock()
-        self.hierarchy_dock = FrmbHierarchyBrowserDock()
-
+        self.main_widget = InnerCentralWidget()
         self.setMenuBar(self.menubar)
-        self.setCentralWidget(self.hierarchy_dock)
-        self.addDockWidget(
-            QtCore.Qt.DockWidgetArea.TopDockWidgetArea, self.control_bar_dock
-        )
+        self.setCentralWidget(self.main_widget)
+        self.setContentsMargins(0, 0, 0, 0)
