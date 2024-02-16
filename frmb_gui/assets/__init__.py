a9d7ac56bf0626dd78f7a33c92e8c3b35f4dc0f6
Liam Collod <lcollod@gmail.com>
2024-02-16 19:26:36 +0100

    feat(assets): add Icon class to public interface

--- frmb_gui/assets/__init__.py
+++ frmb_gui/assets/__init__.py
@@ -1,4 +1,6 @@
 from ._switch import SwitchButton
+from ._icon import StylesheetIconButton
+from ._icon import StylesheetIcon
 from ._headermenu import MainMenuBar
 from ._headerwidget import MainControlBarWidget
 from ._headerwidget import AppTitleWidget
